6b5b94e3beb19f0727496eb4a3a9cec0d9466310
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-09-16 20:47:39 +0930

    trying out OAuth for EventBrite

--- public/js/members.js
+++ public/js/members.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
 
   const eventBriteBtnEl = $("#eventBriteBtn");
-  const eventFrameEl = $("#eventBriteFrame");
-  const eventBriteDivEl = $("#eventBriteDiv");
+  // const eventFrameEl = $("#eventBriteFrame");
+  // const eventBriteDivEl = $("#eventBriteDiv");
 
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
@@ -15,9 +15,23 @@ $(document).ready(() => {
   });
 
   $(eventBriteBtnEl).on("click", () => {
-    console.log("Creating Event Frame");
-    eventFrameEl.attr("src", "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=XRZT3J35M2JMHLMTMMNH&redirect_uri=https://www.eventbrite.com.au/d/australia/motorcycle-show/");
-    eventBriteDivEl.append(eventFrameEl);
+    // console.log("Creating Event Frame");
+    // eventFrameEl.attr("src", "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=XRZT3J35M2JMHLMTMMNH&redirect_uri=https://www.eventbrite.com.au/d/australia/motorcycle-show/");
+    // eventBriteDivEl.append(eventFrameEl);
+    eventBrite();
   });
-  
+
+  function eventBrite() {
+    $.ajax({
+      method: "GET",
+      header: ("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"),
+      url: "https://cors-ut-bootcamp.herokuapp.com/https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=XRZT3J35M2JMHLMTMMNH&redirect_uri=https://www.eventbrite.com.au/",
+      headers: {
+        "Authorization": "Bearer XRZT3J35M2JMHLMTMMNH",
+      }
+    }).then(function(response) {
+      console.log(response);
+    });
+  }
+
 });
